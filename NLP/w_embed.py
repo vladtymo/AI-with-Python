e5f8bd39f5d8daaf179b66d1af866a9bf03ceac0
vtymo <vladyslav_tymoshchuk@trimble.com>
2026-05-18 18:36:53 +0300

    Update

--- NLP/w_embed.py
+++ NLP/w_embed.py
@@ -2,19 +2,19 @@ from gensim.models import Word2Vec
 
 # Приклад корпусу
 sentences = [
-    ["король", "є", "чоловіком"],
-    ["королева", "є", "жінкою"],
-    ["чоловік", "і", "жінка"],
-    ["парламент", "ухвалює", "закони"],
+    ["king", "is", "man"],
+    ["queen", "is", "woman"],
+    ["man", "and", "woman"],
+    ["parliament", "enacts", "laws"],
 ]
 
 # Створення моделі Word2Vec
 model = Word2Vec(sentences, vector_size=50, window=2, min_count=1, workers=4)
 
-# Вектор для слова "король"
-vector = model.wv["король"]
-print("Вектор для слова 'король':\n", vector)
+# Вектор для слова "king"
+vector = model.wv["king"]
+print("Вектор для слова 'king':\n", vector)
 
-# Знайдемо слова, подібні до "король"
-similar = model.wv.most_similar("король")
-print("Схожі слова до 'король':\n", similar)
+# Знайдемо слова, подібні до "king"
+similar = model.wv.most_similar("king")
+print("Схожі слова до 'king':\n", similar)
